64e7901b204db1ebc85f54570aad40fa53302044
Ankit Pathak <workwithankit2002@gmail.com>
2024-08-28 22:43:58 +0530

    Create Solution.js

---
+++ 33. Search in Rotated Sorted Array/Solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    let mid;
+    while(start <= end) {
+        mid = Math.floor(start + (end - start)/2);
+        if(nums[mid] === target) {
+            return mid;
+        } 
+        if(nums[start] <= nums[mid]) {
+            if(nums[start] <= target && target <= nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if(nums[mid] <= target && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
